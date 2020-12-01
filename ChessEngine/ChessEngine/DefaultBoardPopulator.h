4827548ad08303e0f7c1fddb2e8b4aa62ce981b5
Joris van Rheden <jorisvanrheden@hotmail.com>
2020-12-01 12:57:42 +0100

    mirror piece setup instead of rotate

--- ChessEngine/ChessEngine/DefaultBoardPopulator.h
+++ ChessEngine/ChessEngine/DefaultBoardPopulator.h
@@ -18,11 +18,8 @@ public:
 		populatePiecesDefault(board, Coordinate(6, 0), new Knight(PlayerType::BLACK), new Knight(PlayerType::WHITE));
 		populatePiecesDefault(board, Coordinate(7, 0), new Rook(PlayerType::BLACK), new Rook(PlayerType::WHITE));
 
-		board.setPieceAt(Coordinate(3, 0), new Queen(PlayerType::WHITE));
-		board.setPieceAt(Coordinate(4, 0), new King(PlayerType::WHITE));
-
-		board.setPieceAt(Coordinate(3, 7), new Queen(PlayerType::BLACK));
-		board.setPieceAt(Coordinate(4, 7), new King(PlayerType::BLACK));
+		populatePiecesDefault(board, Coordinate(3, 0), new Queen(PlayerType::BLACK), new Queen(PlayerType::WHITE));
+		populatePiecesDefault(board, Coordinate(4, 0), new King(PlayerType::BLACK), new King(PlayerType::WHITE));
 
 		//Add pawns
 		for (int i = 0; i < board.getSizeX(); i++) 
@@ -36,7 +33,7 @@ private:
 	{
 		board.setPieceAt(coordinate, black);
 
-		//rotated
-		board.setPieceAt(Coordinate(board.getSizeX() - coordinate.getX() - 1, board.getSizeY() - coordinate.getY() - 1), white);
+		//mirrored
+		board.setPieceAt(Coordinate(coordinate.getX(), board.getSizeY() - coordinate.getY() - 1), white);
 	}
 };
